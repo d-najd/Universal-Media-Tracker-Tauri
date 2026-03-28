7ccf48a723fd2321a6027be53263518459677d42
d-najd <dimitar.hristijan.najdovski@gmail.com>
2026-03-29 00:26:46 +0100

    feat: android build working

--- vite.config.ts
+++ vite.config.ts
@@ -5,26 +5,27 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+	plugins: [react(), tailwindcss()],
 
-    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-    //
-    // 1. prevent vite from obscuring rust errors
-    clearScreen: false,
-    // 2. tauri expects a fixed port, fail if that port is not available
-    server: {
-        port: 1420,
-        strictPort: true,
-        watch: {
-            // 3. tell vite to ignore watching `src-tauri`
-            ignored: ['**/src-tauri/**']
-        }
-    },
+	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+	//
+	// 1. prevent vite from obscuring rust errors
+	clearScreen: false,
+	// 2. tauri expects a fixed port, fail if that port is not available
+	server: {
+		host: '0.0.0.0',
+		port: 1420,
+		strictPort: true,
+		watch: {
+			// 3. tell vite to ignore watching `src-tauri`
+			ignored: ['**/src-tauri/**']
+		}
+	},
 
-    // Shadcn UI
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    }
+	// Shadcn UI
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	}
 })
