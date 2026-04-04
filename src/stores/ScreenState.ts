d71d9827a4f260bdacb326c27173bc92cc9e3729
d-najd <dimitar.hristijan.najdovski@gmail.com>
2026-04-04 14:32:32 +0200

    feat!: adding dynamic route support

--- src/stores/ScreenState.ts
+++ src/stores/ScreenState.ts
@@ -1,3 +1,6 @@
-/* eslint-disable */
+import { RouteObject } from 'react-router'
+
 // TODO mobx.reaction can be used to notify the SessionScreenStackStore when an screen gets modified
-export default interface ScreenState {}
+export default interface ScreenState {
+	route: RouteObject
+}
