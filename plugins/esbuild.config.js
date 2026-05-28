3a4fe4696ab4ab38639d7ddfb387229d34893d3c
d-najd <dimitar.hristijan.najdovski@gmail.com>
2026-05-28 16:08:00 +0200

    refactor

--- plugins/esbuild.config.js
+++ plugins/esbuild.config.js
@@ -3,43 +3,13 @@ import * as fs from 'node:fs'
 import path from 'path'
 import { globalExternals } from '@fal-works/esbuild-plugin-global-externals'
 
-// const dir = 'src/app/plugins/ts'
 const dir = 'plugins'
-
-function replaceTest(text, replacements) {
-	let result = text
-	for (const [from, to] of Object.entries(replacements)) {
-		const importRegex = new RegExp(
-			`import\\s+?(?:(?:(?:[\\w*\\s{},]*)\\s+from\\s+?)|)(?:(?:"${from}")|(?:'${from}'))[\\s]*?(?:;|$|)`,
-			'g',
-		)
-		console.log(importRegex)
-
-		result = result.replace(importRegex, (fullMatch) => {
-			// Replace just the package name with quotes removed
-			const replacedPackage = fullMatch.replace(
-				new RegExp(`["']${from}["']`),
-				to,
-			)
-
-			return replacedPackage.replace(/^import/, 'var')
-		})
-	}
-	return result
-}
-
-function replaceTest2(text) {
-	return text.replace(
-		/module\.exports = void 0/g,
-		'module.exports = MelancholySdk',
-	)
-}
 
 for (const folder of fs.readdirSync(dir)) {
 	const indexPath = path.join(dir, folder, 'index.ts')
 	if (!fs.existsSync(indexPath)) continue
 
-	const buildResult = await esbuild.build({
+	await esbuild.build({
 		plugins: [
 			globalExternals({
 				react: {
@@ -59,7 +29,7 @@ for (const folder of fs.readdirSync(dir)) {
 					type: 'cjs',
 				},
 				'@d-najd/universal-media-tracker-sdk': {
-					varname: 'MelancholySdk',
+					varName: 'MelancholySdk',
 					type: 'cjs',
 				},
 				'react/jsx-runtime': {
@@ -73,14 +43,8 @@ for (const folder of fs.readdirSync(dir)) {
 		platform: 'node',
 		format: 'esm',
 		sourcemap: false,
-		minify: false,
+		minify: true,
+		outfile: path.join('src/app/plugins/js', `${folder}.js`),
 		jsx: 'transform',
-		write: false,
 	})
-
-	let text = buildResult.outputFiles[0].text
-	text = replaceTest2(text)
-
-	fs.mkdirSync('src/app/plugins/js', { recursive: true })
-	fs.writeFileSync(path.join('src/app/plugins/js', `${folder}.js`), text, {})
 }
