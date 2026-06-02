2f8b33c14fc937b4fb0371f621a845a3d3aa2956
d-najd <dimitar.hristijan.najdovski@gmail.com>
2026-06-02 18:10:53 +0200

    added basic plugin ui

--- plugins/default-ui/features/plugins-manage/index.tsx
+++ plugins/default-ui/features/plugins-manage/index.tsx
@@ -1,16 +1,59 @@
-import { Box } from '@mui/material'
+import {
+	Avatar,
+	Card,
+	CardContent,
+	CardHeader,
+	createTheme,
+	IconButton,
+	Stack,
+	ThemeProvider,
+	Typography,
+} from '@mui/material'
 import plugin from '../..'
+import { Settings } from '@mui/icons-material'
+
+const darkTheme = createTheme({
+	cssVariables: true,
+	shape: {
+		borderRadius: '12px',
+	},
+	palette: {
+		mode: 'dark',
+	},
+})
 
 export default function PluginsManagePage() {
 	const localPluginSpecs = plugin.app.plugin.getLocalPluginConfigs()
 
-	console.log('COUNT IS ' + localPluginSpecs.length)
-
 	return (
-		<>
-			{localPluginSpecs.map((item) => (
-				<Box key={item.id}>{item.name}</Box>
-			))}
-		</>
+		<ThemeProvider theme={darkTheme}>
+			<Stack sx={{ px: 2, py: 2 }} spacing={1.5}>
+				{localPluginSpecs.map((item) => (
+					<Card key={item.id}>
+						<CardHeader
+							avatar={<Avatar src={item.logo} alt={item.name} />}
+							title={item.name}
+							subheader={item.status}
+							action={
+								<IconButton>
+									<Settings />
+								</IconButton>
+							}
+						></CardHeader>
+						<CardContent>
+							<Typography
+								variant="body2"
+								sx={{ color: 'text.secondary' }}
+							>
+								This impressive paella is a perfect party dish
+								and a fun meal to cook together with your
+								guests. Add 1 cup of frozen peas along with the
+								mussels, if you like.
+							</Typography>
+						</CardContent>
+					</Card>
+				))}
+			</Stack>
+		</ThemeProvider>
 	)
 }
